084555c2b80fc934278a7c77c1c95d32e52f8d09
gustavdossant <xspeedbr@gmail.com>
2023-06-23 16:50:33 -0300

    .

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt("Digite um valor: "));
+let contagem = "Contagem: ";
+for (let i = 1; i <= numero; i++) {
+    contagem += i + " ";
+}
+contagem += "Acabou!";
+console.log(contagem);
